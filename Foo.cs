af2d711d0dc493bbe0527c13c70b1437e243b40c
Jeff Morris <jeffry.morris@couchbase.com>
2020-09-25 11:49:20 -0700

    initial commit

---
+++ Foo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Couchbase
+{
+    public class Foo
+    {
+        public string Bar { get; set; }
+
+        public override string ToString()
+        {
+            return Bar ?? "Hello, world.";
+        }
+    }
+}
